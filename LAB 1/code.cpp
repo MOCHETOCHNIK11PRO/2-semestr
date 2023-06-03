25337c47936e6418f07da9ebd0e3f8dccaba6b34
Даниил Каекбердин <114986079+MOCHETOCHNIK11PRO@users.noreply.github.com>
2023-06-03 22:01:12 +0300

    Add files via upload

---
+++ LAB 1/code.cpp
@@ -0,0 +1,419 @@
+﻿#include <iostream> 
+#include <string>  
+#include <Windows.h> 
+
+using namespace std;
+
+struct Students {																			
+	string Firs_Middle_Last_Names;															
+	unsigned int Group_Number, Number_In_Group;												
+	short unsigned int Floor, Alg_And_Geom, Math_Analysis, Programming, Physics,			
+		Pysical_Education, Philosophy, History, Ecology;									
+	float Avarage_Mark;																		
+};
+
+const int Size = 10;																	
+struct Students students[Size];															
+
+void Data_Add(int Quantity) {																
+	SetConsoleCP(1251);																		
+	cout << "Данные студента\n\n"															
+		"Введите ФИО: ";																	
+	getline(cin >> ws, students[Quantity].Firs_Middle_Last_Names);							
+	SetConsoleCP(866);																	
+	
+	while (true) {																			
+		cout << "Введите пол\n"																
+			"1) Мужской\n"																    
+			"2) Женский\n\n"															    
+			"Ответ: ";																       
+		cin >> students[Quantity].Floor;												    
+		if (students[Quantity].Floor == 1 || students[Quantity].Floor == 2) {		        
+			break;																			 
+		}
+	}
+
+	cout << "Введите номер группы: ";														
+	cin >> students[Quantity].Group_Number;													
+	
+	cout << "Введите номер студента в группе: ";											
+	cin >> students[Quantity].Number_In_Group;												
+
+	cout << "\n\nОцнки за прошлую сессию\n\n"												
+		"Алгебра и геометрия: ";															
+	cin >> students[Quantity].Alg_And_Geom;													
+	while (students[Quantity].Alg_And_Geom < 2 || students[Quantity].Alg_And_Geom > 5) {	
+		cout << "???";																		
+		cin >> students[Quantity].Alg_And_Geom;												
+	}
+
+	cout << "Математический анализ: ";														
+	cin >> students[Quantity].Math_Analysis;												
+	while (students[Quantity].Math_Analysis < 2 || students[Quantity].Math_Analysis > 5) {  
+		cout << "???";																		
+		cin >> students[Quantity].Math_Analysis;											
+	}
+
+	cout << "Программирование: ";															
+	cin >> students[Quantity].Programming;													
+	while (students[Quantity].Programming < 2 || students[Quantity].Programming > 5) {		
+		cout << "???";																		
+		cin >> students[Quantity].Programming;												
+	}
+
+	cout << "Физика: ";																		
+	cin >> students[Quantity].Physics;														
+	while (students[Quantity].Physics < 2 || students[Quantity].Physics > 5) {				
+		cout << "???";																		
+		cin >> students[Quantity].Physics;													
+	}
+
+	cout << "Физ-ра: ";																		
+	cin >> students[Quantity].Pysical_Education;											
+	while (students[Quantity].Pysical_Education < 2 || students[Quantity].Pysical_Education > 5) { 
+		cout << "???";																			  
+		cin >> students[Quantity].Pysical_Education;											 
+	}
+
+	cout << "Философия: ";																	
+	cin >> students[Quantity].Philosophy;													
+	while (students[Quantity].Philosophy < 2 || students[Quantity].Philosophy > 5) {	
+		cout << "???";																		
+		cin >> students[Quantity].Philosophy;												
+	}
+
+	cout << "История: ";															
+	cin >> students[Quantity].History;														
+	while (students[Quantity].History < 2 || students[Quantity].History > 5) {			
+		cout << "???";																		
+		cin >> students[Quantity].History;													
+	}
+
+	cout << "Экология: ";																
+	cin >> students[Quantity].Ecology;														
+	while (students[Quantity].Ecology < 2 || students[Quantity].Ecology > 5) {			
+		cout << "???";																		
+		cin >> students[Quantity].Ecology;													
+	}
+																							
+	students[Quantity].Avarage_Mark = (students[Quantity].Alg_And_Geom + students[Quantity].Math_Analysis + students[Quantity].Programming + students[Quantity].Physics + students[Quantity].Pysical_Education + students[Quantity].Philosophy + students[Quantity].History + students[Quantity].Ecology) / 8;
+	system("pause");																		
+}
+
+void Data_Correct(int Quantity) {																				
+	short unsigned int Correct_Number, Choice;																	
+	cout << "Введите номер студента который хотите исправить: ";													
+	while (true) {																									
+		cin >> Correct_Number;																					
+		Correct_Number--;																						
+		system("cls");																								
+		if (Correct_Number < Quantity) {																		
+			while (true) {																							
+				cout << "\n   Выбирете хотите исправить:"															
+					"\n1) ФИО"																						
+					"\n2) Пол"																						
+					"\n3) Номер группы"																				
+					"\n4) Номер в группе"																			
+					"\n   Оценки за:"																				
+					"\n5) Алгебра и геометрия"																		
+					"\n6) Математический анализ"																	
+					"\n7) Программирование"																			
+					"\n8) Физика"																					
+					"\n9) Физ-ра"																					
+					"\n10) Философия"																				
+					"\n11) История"																					
+					"\n12) Экология"																				
+					"\n\n13) Назад"																					
+					"\n0) Выход"																					
+					"\n\nОтвет: ";																					
+																													
+				cin >> Choice;																						
+				system("cls");																						
+				cout << "Исправление: ";																			
+																													
+				if (Choice == 0 || Choice == 13) {																	
+					break;																							
+				}																									
+				if (Choice == 1) {																					
+					SetConsoleCP(1251);																				
+					getline(cin >> ws, students[Correct_Number].Firs_Middle_Last_Names);							
+					SetConsoleCP(866);																				
+				}																									
+				if (Choice == 2) {																					
+					while (true) {																					
+						cout << "\n"																				
+							"1) Мужской\n"																			
+							"2) Женский\n\n"																		
+							"Ответ: ";																				
+						cin >> students[Correct_Number].Floor;														
+						if (students[Correct_Number].Floor == 1 || students[Correct_Number].Floor == 2) {			
+							break;																					
+						}																					
+					}																								
+				}																									
+				if (Choice == 3) {
+					cin >> students[Correct_Number].Group_Number;
+				}
+				if (Choice == 4) {
+					cin >> students[Correct_Number].Number_In_Group;
+				}
+				if (Choice == 5) {
+					cin >> students[Correct_Number].Alg_And_Geom;
+					while (students[Correct_Number].Alg_And_Geom < 2 || students[Correct_Number].Alg_And_Geom > 5) {
+						cout << "???";
+						cin >> students[Correct_Number].Alg_And_Geom;
+					}
+				}
+				if (Choice == 6) {
+					cin >> students[Correct_Number].Math_Analysis;
+					while (students[Correct_Number].Math_Analysis < 2 || students[Correct_Number].Math_Analysis > 5) {
+						cout << "???";
+						cin >> students[Correct_Number].Math_Analysis;
+					}
+				}
+				if (Choice == 7) {
+					cin >> students[Correct_Number].Programming;
+					while (students[Correct_Number].Programming < 2 || students[Correct_Number].Programming > 5) {
+						cout << "???";
+						cin >> students[Correct_Number].Programming;
+					}
+				}
+				if (Choice == 8) {
+					cin >> students[Correct_Number].Physics;
+					while (students[Correct_Number].Physics < 2 || students[Correct_Number].Physics > 5) {
+						cout << "???";
+						cin >> students[Correct_Number].Physics;
+					}
+				}
+				if (Choice == 9) {
+					cin >> students[Correct_Number].Pysical_Education;
+					while (students[Correct_Number].Pysical_Education < 2 || students[Correct_Number].Pysical_Education > 5) {
+						cout << "???";
+						cin >> students[Correct_Number].Pysical_Education;
+					}
+				}
+				if (Choice == 10) {
+					cin >> students[Correct_Number].Philosophy;
+					while (students[Correct_Number].Philosophy < 2 || students[Correct_Number].Philosophy > 5) {
+						cout << "???";
+						cin >> students[Correct_Number].Philosophy;
+					}
+				}
+				if (Choice == 11) {
+					cin >> students[Correct_Number].History;
+					while (students[Correct_Number].History < 2 || students[Correct_Number].History > 5) {
+						cout << "???";
+						cin >> students[Correct_Number].History;
+					}
+				}
+				if (Choice == 12) {
+					cin >> students[Correct_Number].Ecology;
+					while (students[Correct_Number].Ecology < 2 || students[Correct_Number].Ecology > 5) {
+						cout << "???";
+						cin >> students[Correct_Number].Ecology;
+					}
+				}
+				if (Choice >= 5 && Choice <= 12) {																	
+					students[Correct_Number].Avarage_Mark = (students[Correct_Number].Alg_And_Geom + students[Correct_Number].Math_Analysis + students[Correct_Number].Programming + students[Correct_Number].Physics + students[Correct_Number].Pysical_Education + students[Correct_Number].Philosophy + students[Correct_Number].History + students[Correct_Number].Ecology) / 8;
+				}
+			}
+		}
+		if (Correct_Number >= Quantity) {
+			cout << "???";																					
+		}
+		if (Choice == 0) {
+			break;
+		}
+	}
+}
+
+void Output(int i) {																							
+	cout << "Номер записи: "<< i+1
+		<< "\n\nФИО: " << students[i].Firs_Middle_Last_Names;
+	if (students[i].Floor == 1)
+	{
+		cout << "\nПол: Мужской";
+	}
+	else
+	{
+		cout << "\nПол: Женский";
+	}
+	cout << "\nГруппа: " << students[i].Group_Number
+		<< "\nНомер в группе: " << students[i].Number_In_Group
+		<< "\n\n\nОценки\n\n"
+		"Алгебра и геометрия: " << students[i].Alg_And_Geom
+		<< "\nМатематический анализ: " << students[i].Math_Analysis
+		<< "\nПрограммирование: " << students[i].Programming
+		<< "\nФизика: " << students[i].Physics 
+		<< "\nФиз-ра: " << students[i].Pysical_Education
+		<< "\nФилософия: " << students[i].Philosophy
+		<< "\nИстория: " << students[i].History
+		<< "\nЭкология: " << students[i].Ecology
+		<< "\n________________________________________________________________________________________________\n\n";
+}
+
+void All_Data_Output(int Quantity) {																		
+	for (int i = 0; i < Quantity; i++) {																			
+		Output(i);																									
+	}
+	system("pause");
+}
+
+void Output_To_Group(int Quantity) {																			
+	unsigned int Group_Number, Count;																				
+	Count = 0;																										
+	cout << "Введите номер группы: ";																				
+	cin >> Group_Number;																					
+
+	for (int i = 0; i < Quantity; i++) {																		
+		if (students[i].Group_Number == Group_Number) {																
+			Output(i);																								
+			Count++;																								
+		}
+	}
+	if (Count == 0) {
+		cout << "Группа не найдена...\n";
+	}
+	system("pause");
+}
+
+void Men_Women_Count(int Quantity) {																			
+	int Men = 0;																									
+	int Women = 0;																								
+	for (int i = 0; i < Quantity; i++) {																		
+		if (students[i].Floor == 1) {																				
+			Men++;																							
+		}																										
+		if (students[i].Floor == 2) {																
+			Women++;																								
+		}																											
+	}																												
+	cout << Men << " cтудентов мужского пола\n"																		
+		<< Women << " студентов женского пола\n";																	
+	system("pause");
+}
+
+void Genius_Output(int Quantity) {																				
+	short unsigned int Count;																				
+	Count = 0;																								
+	for (int i = 0; i < Quantity; i++) {																	
+		if (students[i].Avarage_Mark == 5) {
+			Output(i);																								
+			Count++;																						
+		}
+	}
+	if (Count == 0) {
+		cout << "Такие студенты не найдены...\n";
+	}
+	system("pause");
+}
+
+void Normis_Output(int Quantity) {																				
+	short unsigned int Count;
+	Count = 0;
+	for (int i = 0; i < Quantity; i++) {
+		if (students[i].Avarage_Mark < 5 && students[i].Avarage_Mark >= 4 && students[i].Alg_And_Geom != 3 && students[i].Math_Analysis != 3 && students[i].Programming != 3 && students[i].Physics != 3 && students[i].Pysical_Education != 3 && students[i].Philosophy != 3 && students[i].History != 3 && students[i].Ecology != 3 && students[i].Alg_And_Geom != 2 && students[i].Math_Analysis != 2 && students[i].Programming != 2 && students[i].Physics != 2 && students[i].Pysical_Education != 2 && students[i].Philosophy != 2 && students[i].History != 2 && students[i].Ecology != 2) {
+			Output(i);
+			Count++;
+		}
+	}
+	if (Count == 0) {
+		cout << "Такие студенты не найдены...\n";
+	}
+	system("pause");
+}
+
+void No_Scholarship_Output(int Quantity) {																			
+	short unsigned int Count;
+	Count = 0;
+	for (int i = 0; i < Quantity; i++) {
+		if (students[i].Alg_And_Geom == 3 || students[i].Math_Analysis == 3 || students[i].Programming == 3 || students[i].Physics == 3 || students[i].Pysical_Education == 3 || students[i].Philosophy == 3 || students[i].History == 3 || students[i].Ecology == 3 || students[i].Alg_And_Geom == 2 || students[i].Math_Analysis == 2 || students[i].Programming == 2 || students[i].Physics == 2 || students[i].Pysical_Education == 2 || students[i].Philosophy == 2 || students[i].History == 2 || students[i].Ecology == 2) {
+			Output(i);
+			Count++;
+		}
+	}
+	if (Count == 0) {
+		cout << "Такие студенты не найдены...\n";
+	}
+	system("pause");
+}
+
+void Number_In_List_Output(int Quantity) {																		
+	unsigned int Number_In_Group, Count;
+	Count = 0;
+	cout << "Введите номер в группе: ";
+	cin >> Number_In_Group;
+
+	for (int i = 0; i < Quantity; i++) {
+		if (students[i].Number_In_Group == Number_In_Group) {
+			Output(i);
+			Count++;
+		}
+	}
+	if (Count == 0) {
+		cout << "Группа не найдена...\n";
+	}
+	system("pause");
+}
+
+int main()
+{	
+	setlocale(0, "Rus");																		
+	
+	short unsigned int Choice, Quantity;														
+	Quantity = 0;
+
+	while (true) {
+		system("cls");
+		cout << "\tМеню:\n\n"
+			"1)Создать запись\n"
+			"2)Внести изменения\n"
+			"3)Вывод всех данных\n"
+			"4)Вывод данных о студентах определенной группы\n"
+			"5)Вывод топа самых успешных студентов за прошлую сессию\n"
+			"6)Вывод количества студентов мужского и женского\n"
+			"7)Вывод успеваемости\n"
+			"8)Вывод дынных о студентах по номеру в группе\n\n"
+			"0)Выход\n\n"
+			"Ответ: ";
+		
+		cin >> Choice;
+		system("cls");
+
+		if (Choice == 0) {
+			break;
+		}
+		if (Choice == 1) {																	
+			Data_Add(Quantity);																	
+			Quantity++;																			
+		}
+		if (Choice == 2) {
+			Data_Correct(Quantity);
+		}
+		if (Choice == 3) {
+			All_Data_Output(Quantity);
+		}
+		if (Choice == 4) {
+			Output_To_Group(Quantity);
+		}
+		if (Choice == 5) {
+			Genius_Output(Quantity);
+		}
+		if (Choice == 6) {
+			Men_Women_Count(Quantity);
+		}
+		if (Choice == 7) {
+			cout << "Отличники:\n\n";
+			Genius_Output(Quantity);
+			cout << "\n\nХорошисты:\n\n";
+			Normis_Output(Quantity);
+			cout << "\n\nБез стипендии:\n\n";
+			No_Scholarship_Output(Quantity);
+		}
+		if (Choice == 8) {
+			Number_In_List_Output(Quantity);
+		}
+	}
+	return 0;
+}
+
---
+++ LAB 2/Code.cpp
@@ -0,0 +1,340 @@
+﻿#include <iostream>
+#include <chrono>
+#include <ctime>
+
+using namespace std;
+using namespace chrono;
+
+struct Node {
+	int Data;
+	Node* Previous = NULL, * Next = NULL;
+	Node(int Data) {
+		this->Data = Data;
+		this->Previous = this->Next;
+	}
+};
+
+struct Double_Linked_List {
+	Node* Head  = NULL, * Tail = NULL;
+	
+	void Output() {
+		Node* Element = Head;
+		while (Element != NULL) {
+			cout << Element->Data << " ";
+			Element = Element->Next;
+		}
+		cout << endl;
+	}
+	
+	Node* Create_Double_Linked_List(int data) {
+		Node* Current_Element = new Node(data);
+		Current_Element->Previous = Tail;
+		if (Tail != NULL) {
+			Tail->Next = Current_Element;
+		}
+		if (Head == NULL) {
+			Head = Current_Element;
+		}
+		Tail = Current_Element;
+		return Current_Element;
+	}
+	Node* Get_Element(int Index) {
+		{
+			Node* Current_Element = Head;
+			int Count = 0;
+			while (Count != Index)
+			{
+				if (Current_Element == NULL)
+					return Current_Element;
+				Current_Element = Current_Element->Next;
+				Count++;
+			}
+			return Current_Element;
+		}
+	}
+
+	Node* Insert_Element(int Index, int Data) {
+		Node* Right = Get_Element(Index);
+		if (Right == NULL) {
+			Node* Current_Element = new Node(Data);
+			Current_Element->Data = Data;
+			Current_Element->Previous = Tail;
+			Tail->Next = Current_Element;
+			Current_Element->Next = 0;
+			Tail = Current_Element;
+			return Current_Element;
+		}
+		
+		Node* Left = Right->Previous;
+		if (Left == NULL)
+		{
+			Node* Current_Element = new Node(Data);
+			Current_Element->Data = Data;
+			Current_Element->Next = Head;
+			Head->Previous = Current_Element;
+			Current_Element->Previous = 0;
+			Head = Current_Element;
+			return Current_Element;
+		}
+
+		Node* Current_Element = new Node(Data);
+		Current_Element->Next = Right;
+		Current_Element->Previous = Left;
+		Left->Next = Current_Element;
+		Right->Previous = Current_Element;
+		Current_Element->Data = Data;
+		return Current_Element;
+	}
+
+	Node* Delete(Node* var) {
+		Node* Current_Element = var;
+		if (Current_Element->Previous == NULL)
+		{
+			Node* Element = Head->Next;
+			Element->Previous = NULL;
+			delete Head;
+			Head = Element;
+			return Element;
+		}
+		if (Current_Element->Next == NULL)
+		{
+			Node* Element = Tail->Previous;
+			Element->Next = NULL;
+			delete Tail;
+			Tail = Element;
+		}
+		if (Current_Element->Next != 0 and Current_Element->Previous != 0)
+		{
+			Node* Left = Current_Element->Previous;
+			Node* Right = Current_Element->Next;
+			Left->Next = Right;
+			Right->Previous = Left;
+			delete Current_Element;
+		}
+	}
+	
+	void Delete_Element_By_Index(int Index) {
+		Delete(Get_Element(Index));
+	}
+
+
+	void Swap_Elements(int Index_1, int Index_2) {
+		Node* First_Element = Head, * Second_Element = Head;
+		for (int i = 0; i < Index_1; i++) {
+			First_Element = First_Element->Next;
+		}
+		for (int i = 0; i < Index_2; i++) {
+			First_Element = First_Element->Next;
+		}
+		int Temporary_Variable = First_Element->Data;
+		First_Element->Data = Second_Element->Data;
+		Second_Element->Data = Temporary_Variable;
+	}
+};
+int main()
+{
+	setlocale(0, "");
+	srand(time(NULL));
+	unsigned short int Choise, List_Size, Elemet_Number = 0, Flag = 0;
+	int Data;
+	unsigned int Unsigned_Data, Index, Index_;
+	Double_Linked_List list;
+	time_point<steady_clock> start;
+	time_point<steady_clock> end;
+	while (true)
+	{
+		cout << "   Меню:\n\n"
+			"1) Сформировать двусвязанный список.\n"
+			"2) Вставить элемент.\n"
+			"3) Удалить элемент.\n"
+			"4) Обменять элементы\n"
+			"5) Получить элемент.\n\n"
+			"0) Выход.\n\n"
+			"Ответ: ";
+		cin >> Choise;
+		system("cls");
+		
+		if (Choise == 0)
+		{
+			break;
+		}
+		
+		if (Choise != 1 && Flag == 0) {
+			Choise = 1;
+			cout << "Для начала сформируйте список.\n\n";
+		}
+
+		switch (Choise){
+		case 1:
+			
+			Flag = 1;
+
+			while (true){
+				cout << "1) Ввести элементы в ручную (только положительные числа).\n"
+					"2) Сгенерировать список рандомно от 0 до 99.\n\n"
+					"0) Выход.\n\n"
+					"Ответ: ";
+				cin >> Choise;
+				
+				system("cls");
+
+				if (Choise == 0)
+				{
+					break;
+				}
+				
+				switch (Choise)
+				{
+				case 1:
+					
+					start = chrono::steady_clock::now();
+					
+					while (true) {
+						cout << "Введите элемет списка: ";
+						cin >> Data;
+						
+						if (Data < 0) {
+							break;
+						}
+						
+						Elemet_Number++;
+						list.Create_Double_Linked_List(Data);
+						system("cls");
+						cout << "Чтобы завершить ввод введите отрицательное число\n\n";
+					}
+					
+					end = chrono::steady_clock::now();
+					
+					list.Output();
+					cout << "Затраченное время: " << duration_cast<microseconds>(end - start).count() << " мкс.\n";
+					system("pause");
+					break;
+				case 2:
+					cout << "Размер списка: ";
+					cin >> List_Size;
+					system("cls");
+					
+					start = chrono::steady_clock::now();
+					
+					while (Elemet_Number < List_Size) {
+						Data = rand() % 100;
+						list.Create_Double_Linked_List(Data);
+						Elemet_Number++;
+					}
+					
+					end = chrono::steady_clock::now();
+					
+					list.Output();
+					cout << "Затраченное время: " << duration_cast<microseconds>(end - start).count() << " мкс.\n";
+					system("pause");
+					system("cls");
+					break;
+				}
+			}
+			break;
+		case 2:
+			while (true)
+			{
+				cout << "Введите индекс: ";
+				cin >> Index;
+				Index--;
+				system("cls");
+				if (Index < Elemet_Number) {
+					break;
+				}
+				cout << "Введите индекс от 1 до " << Elemet_Number << "\n\n";
+			}
+			
+			cout << "Введите значение элеметa: ";
+			cin >> Unsigned_Data;
+			system("cls");
+			
+			start = chrono::steady_clock::now();
+			
+			list.Insert_Element(Index, Unsigned_Data);
+
+			end = chrono::steady_clock::now();
+
+			list.Output();
+			cout << "Затраченное время: " << duration_cast<microseconds>(end - start).count() << " мкс.\n";
+			system("pause");
+			system("cls");
+			Elemet_Number++;
+			break;
+		case 3:
+			while (true)
+			{
+				cout << "Введите индекс: ";
+				cin >> Index;
+				Index--;
+				system("cls");
+				if (Index < Elemet_Number) {
+					break;
+				}
+				cout << "Введите индекс от 1 до " << Elemet_Number << "\n\n";
+			}
+			
+			start = chrono::steady_clock::now();
+			
+			list.Delete_Element_By_Index(Index);
+
+			end = chrono::steady_clock::now();
+
+			list.Output();
+			cout << "Затраченное время: " << duration_cast<microseconds>(end - start).count() << " мкс.\n";
+			system("pause");
+			system("cls");
+			Elemet_Number--;
+			break;
+		case 4:
+
+			while (true)
+			{
+				cout << "Введите индексы: ";
+				cin >> Index >> Index_;
+				Index--;
+				Index_--;
+				if (Index < Elemet_Number && Index_ < Elemet_Number && Index != Index_) {
+					break;
+				}
+				cout << "Введите индекс от 1 до " << Elemet_Number << "\nИндексы не должны быть равными\n\n.";
+			}
+			
+			start = chrono::steady_clock::now();
+
+			list.Swap_Elements(Index, Index_);
+
+			end = chrono::steady_clock::now();
+
+			list.Output();
+			cout << "Затраченное время: " << duration_cast<microseconds>(end - start).count() << " мкс.\n";
+			system("pause");
+			system("cls");
+			break;
+		case 5:
+			while (true)
+			{
+				cout << "Введите индекс: ";
+				cin >> Index;
+				Index--;
+				system("cls");
+				if (Index < Elemet_Number) {
+					break;
+				}
+				cout << "Введите индекс от 1 до " << Elemet_Number << "\n\n";
+			}
+			start = chrono::steady_clock::now();
+
+			cout <<"Итого:" << list.Get_Element(Index)->Data;
+
+			end = chrono::steady_clock::now();
+			cout << "Затраченное время: " << duration_cast<microseconds>(end - start).count() << " мкс.\n";
+			system("pause");
+			system("cls");
+			break;
+		default:
+			cout << "???";
+			break;
+		}
+	}
+}
